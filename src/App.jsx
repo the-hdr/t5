9542402fb0ebc709bc3b5f64564a5074032c1c62
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2025-03-29 17:32:58 +0530

    add board

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Square ({ value, handleClick }) {
+	
+	function handleClick() {
+		console.log ("clicked on " + value);
+	}
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<button className="square" onClick={handleClick}> {value} </button>
+	);
 }
 
-export default App
+function Board () {
+	return (
+		<>
+			<div className="board-row">
+				<Square value={1} />
+				<Square value={2} />
+				<Square value={3} />
+			</div>
+
+			<div className="board-row">
+				<Square value={4} />
+				<Square value={5} />
+				<Square value={6} />
+			</div>
+
+			<div className="board-row">
+				<Square value={7} />
+				<Square value={8} />
+				<Square value={9} />
+			</div>
+		</>
+	);
+}
+
+export default Board;
